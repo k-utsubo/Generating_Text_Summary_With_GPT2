875bb0726a19004f08ae76087780bdc15c9eeaef
Katsuhiko Utsubo <utsuboka@gmail.com>
2021-01-18 14:16:45 +0900

    modify execute script

--- cnn.sh
+++ cnn.sh
@@ -6,9 +6,13 @@ fi
 source venv/bin/activate
 pip install -r requirements.txt
 
+if [ ! -f CNN/CNN_file_size.pickle ];then
 python max_article_sizes.py  $DATA/cnn-dailymail/cnn_stories_tokenized/  CNN
+fi
 
+if [ ! -f CNN/ids.json ];then
 python prepare_data.py  $DATA/cnn-dailymail/cnn_stories_tokenized/  CNN CNN/CNN_file_size.pickle 
+fi
 
-python train_gpt2_summarizer.py --batch_size 1 --root_dir CNN/gpt2_1024_data/
+python train_gpt2_summarizer.py --device cpu --batch_size 1 --root_dir CNN/gpt2_1024_data/
 
